8e22e8770f8e7d318fa9e4ae187207b516324fa1
Brian Guarraci <bguarraci@twitter.com>
2012-12-06 16:31:35 -0800

    fix lispchar bug

--- src/main/scala/org/starlisp/core/LispChar.scala
+++ src/main/scala/org/starlisp/core/LispChar.scala
@@ -5,13 +5,13 @@ import collection.mutable
 object LispChar {
   val lowCache = {
     val lc = new Array[LispChar](256)
-    (0 until 255).foreach{idx => lc(idx) = new LispChar(idx.asInstanceOf[Char])}
+    (0 until 256).foreach{idx => lc(idx) = new LispChar(idx.asInstanceOf[Char])}
     lc
   }
 
   val cache = new mutable.HashMap[Char, LispChar]()
   def create(ch: Char) = {
-    if (ch <= 255) {
+    if (ch < 256) {
       lowCache(ch)
     } else {
       cache.getOrElseUpdate(ch, new LispChar(ch))
